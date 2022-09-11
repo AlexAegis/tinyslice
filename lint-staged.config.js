0cc62e66cee491e9a3cd11d5b15125962acec7ad
AlexAegis <alexaegis@gmail.com>
2022-09-11 19:32:50 +0200

    ci: fix baseHref

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,7 @@
 // For the glob features check out https://github.com/micromatch/micromatch
 module.exports = {
 	'*.(ts|js)': ['eslint --max-warnings=0', 'prettier --check'],
-	'*.css': ['stylelint', 'prettier --list-different'],
-	'*.scss': ['stylelint --syntax=scss', 'prettier --check'],
+	'*.(css|scss)': ['stylelint', 'prettier --check'],
 	'(*.json|.eslintrc|.prettierrc|.stylelintrc|.markdownlintrc)': [('eslint', 'prettier --check')],
 	'*.md': ["markdownlint --ignore 'CHANGELOG.md' --ignore-path '.gitignore'", 'prettier --check'],
 	'*.(yml|yaml)': ['prettier --check'],
