95b471cb7e12b17427727ae01138a80c209d51f4
AlexAegis <alexaegis@gmail.com>
2022-09-29 00:30:00 +0200

    test: add tests for set and update

--- libs/core/src/lib/store/slice.class.spec.ts
+++ libs/core/src/lib/store/slice.class.spec.ts
@@ -28,29 +28,34 @@ describe('slice', () => {
 
 		interface FooState {
 			bar: string;
+			bor: string;
 		}
 
 		let rootSlice!: RootSlice<RootState>;
 		let fooSlice!: Slice<RootState, FooState>;
 		let barSlice!: Slice<FooState, string>;
+		let borSlice!: Slice<FooState, string>;
 
 		const initialRootSlice: RootState = {
-			foo: { bar: 'zed' },
+			foo: { bar: 'zed', bor: 'yon' },
 		};
 
 		const rootObserver: Observer<RootState> = createMockObserver();
 		const fooSliceObserver: Observer<FooState> = createMockObserver();
 		const barSliceObserver: Observer<string> = createMockObserver();
+		const borSliceObserver: Observer<string> = createMockObserver();
 
 		beforeEach(() => {
 			rootSlice = scope.createRootSlice<RootState>(initialRootSlice);
 
 			fooSlice = rootSlice.slice('foo');
 			barSlice = fooSlice.slice('bar');
+			borSlice = fooSlice.slice('bor');
 
 			sink.add(rootSlice.subscribe(rootObserver));
 			sink.add(fooSlice.subscribe(fooSliceObserver));
 			sink.add(barSlice.subscribe(barSliceObserver));
+			sink.add(borSlice.subscribe(borSliceObserver));
 		});
 
 		it('should complete all subscribers if the root store is shut down', () => {
@@ -66,6 +71,10 @@ describe('slice', () => {
 			expect(barSliceObserver.next).toHaveBeenCalledTimes(1);
 			expect(barSliceObserver.error).toHaveBeenCalledTimes(0);
 			expect(barSliceObserver.complete).toHaveBeenCalledTimes(1);
+
+			expect(borSliceObserver.next).toHaveBeenCalledTimes(1);
+			expect(borSliceObserver.error).toHaveBeenCalledTimes(0);
+			expect(borSliceObserver.complete).toHaveBeenCalledTimes(1);
 		});
 
 		it('should complete all child slices if the slice is unsubscribed but not the parent', () => {
@@ -81,6 +90,10 @@ describe('slice', () => {
 			expect(barSliceObserver.next).toHaveBeenCalledTimes(1);
 			expect(barSliceObserver.error).toHaveBeenCalledTimes(0);
 			expect(barSliceObserver.complete).toHaveBeenCalledTimes(1);
+
+			expect(borSliceObserver.next).toHaveBeenCalledTimes(1);
+			expect(borSliceObserver.error).toHaveBeenCalledTimes(0);
+			expect(borSliceObserver.complete).toHaveBeenCalledTimes(1);
 		});
 
 		it('should complete the leaf slices if the slice is unsubscribed but not the parents', () => {
@@ -96,8 +109,131 @@ describe('slice', () => {
 			expect(barSliceObserver.next).toHaveBeenCalledTimes(1);
 			expect(barSliceObserver.error).toHaveBeenCalledTimes(0);
 			expect(barSliceObserver.complete).toHaveBeenCalledTimes(1);
+
+			expect(borSliceObserver.next).toHaveBeenCalledTimes(1);
+			expect(borSliceObserver.error).toHaveBeenCalledTimes(0);
+			expect(borSliceObserver.complete).toHaveBeenCalledTimes(0);
 		});
 	});
+
+	describe('emission', () => {
+		interface RootState {
+			foo: FooState;
+		}
+
+		interface FooState {
+			bar: string;
+			bor: string;
+		}
+
+		let rootSlice!: RootSlice<RootState>;
+		let fooSlice!: Slice<RootState, FooState>;
+		let barSlice!: Slice<FooState, string>;
+		let borSlice!: Slice<FooState, string>;
+
+		const initialRootSlice: RootState = {
+			foo: { bar: 'zed', bor: 'yon' },
+		};
+
+		const rootObserver: Observer<RootState> = createMockObserver();
+		const fooSliceObserver: Observer<FooState> = createMockObserver();
+		const barSliceObserver: Observer<string> = createMockObserver();
+		const borSliceObserver: Observer<string> = createMockObserver();
+
+		beforeEach(() => {
+			rootSlice = scope.createRootSlice<RootState>(initialRootSlice);
+
+			fooSlice = rootSlice.slice('foo');
+			barSlice = fooSlice.slice('bar');
+			borSlice = fooSlice.slice('bor');
+
+			sink.add(rootSlice.subscribe(rootObserver));
+			sink.add(fooSlice.subscribe(fooSliceObserver));
+			sink.add(barSlice.subscribe(barSliceObserver));
+			sink.add(borSlice.subscribe(borSliceObserver));
+		});
+
+		describe('the premade set action', () => {
+			it('should require all fields on a slice and emit all parents and all children', () => {
+				fooSlice.set({ bor: 'bon', bar: 'ber' });
+				expect(rootObserver.next).toHaveBeenCalledTimes(2);
+				expect(rootObserver.error).toHaveBeenCalledTimes(0);
+				expect(rootObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(fooSliceObserver.next).toHaveBeenCalledTimes(2);
+				expect(fooSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(fooSliceObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(barSliceObserver.next).toHaveBeenCalledTimes(2);
+				expect(barSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(barSliceObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(borSliceObserver.next).toHaveBeenCalledTimes(2);
+				expect(borSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(borSliceObserver.complete).toHaveBeenCalledTimes(0);
+			});
+		});
+
+		describe('the premade update action', () => {
+			it('should emit itself and nothing else with an empty update', () => {
+				rootSlice.update({});
+				expect(rootObserver.next).toHaveBeenCalledTimes(2);
+				expect(rootObserver.error).toHaveBeenCalledTimes(0);
+				expect(rootObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(fooSliceObserver.next).toHaveBeenCalledTimes(1);
+				expect(fooSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(fooSliceObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(barSliceObserver.next).toHaveBeenCalledTimes(1);
+				expect(barSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(barSliceObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(borSliceObserver.next).toHaveBeenCalledTimes(1);
+				expect(borSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(borSliceObserver.complete).toHaveBeenCalledTimes(0);
+			});
+
+			it('should emit all parents and all changed children', () => {
+				fooSlice.update({ bor: 'bon' });
+				expect(rootObserver.next).toHaveBeenCalledTimes(2);
+				expect(rootObserver.error).toHaveBeenCalledTimes(0);
+				expect(rootObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(fooSliceObserver.next).toHaveBeenCalledTimes(2);
+				expect(fooSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(fooSliceObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(barSliceObserver.next).toHaveBeenCalledTimes(1);
+				expect(barSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(barSliceObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(borSliceObserver.next).toHaveBeenCalledTimes(2);
+				expect(borSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(borSliceObserver.complete).toHaveBeenCalledTimes(0);
+			});
+
+			it('should emit all parents but no siblings', () => {
+				barSlice.update('updated');
+				expect(rootObserver.next).toHaveBeenCalledTimes(2);
+				expect(rootObserver.error).toHaveBeenCalledTimes(0);
+				expect(rootObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(fooSliceObserver.next).toHaveBeenCalledTimes(2);
+				expect(fooSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(fooSliceObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(barSliceObserver.next).toHaveBeenCalledTimes(2);
+				expect(barSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(barSliceObserver.complete).toHaveBeenCalledTimes(0);
+
+				expect(borSliceObserver.next).toHaveBeenCalledTimes(1);
+				expect(borSliceObserver.error).toHaveBeenCalledTimes(0);
+				expect(borSliceObserver.complete).toHaveBeenCalledTimes(0);
+			});
+		});
+	});
+
 	describe('slices', () => {
 		interface RootState {
 			foo: FooState;
