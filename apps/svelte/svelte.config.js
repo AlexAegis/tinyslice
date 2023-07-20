6ed3ed37323f0c72da654da6dce85a3745b42efc
AlexAegis <alexaegis@gmail.com>
2023-07-20 22:19:30 +0200

    fix: add baseHref

--- apps/svelte/svelte.config.js
+++ apps/svelte/svelte.config.js
@@ -1,6 +1,29 @@
-// managed-by-autotool
+// TODO revert anaged-by-autotool
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/**
+ * TODO: move this to js-tooling
+ * @param {string} path
+ * @returns {path is `/${string}`}
+ */
+const isAbsolute = (path) => {
+	return path.startsWith('/');
+};
+
+/**
+ * TODO: move this to js-tooling
+ * @param {string | undefined} path
+ * @returns {`/${string}` | ''}
+ */
+const toBaseHref = (path = '') => {
+	const trimmed = path.replace(/\/$/, ''); // Must not end with '/'
+	if (trimmed) {
+		return isAbsolute(trimmed) ? trimmed : `/${trimmed}`;
+	} else {
+		return '';
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -14,6 +37,9 @@ export default {
 		},
 	},
 	kit: {
+		paths: {
+			base: toBaseHref(process.env['BASE_HREF']),
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
